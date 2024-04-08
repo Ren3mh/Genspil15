6f8b55f09346aba827f8ce95ac7785f46110fc5b
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-04-08 14:19:30 +0200

    Tilføjet Default Values til Properties i Game.cs

--- Genspil15/Game.cs
+++ Genspil15/Game.cs
@@ -9,34 +9,37 @@ namespace Genspil15
 {
     public class Game
     {
-        public string GameName { get; set; }
-        public string GameEdition { get; set; }
-        public string Genre { get; set; }
-        public int NumberOfPlayersMin { get; set; }
-        public int NumberOfPlayersMax { get; set; }
-        public double Price { get; set; }
-        public int Condition { get; set; }
-        public int QuantityOfGame { get; set; }
-        public string[] Waitlist { get; set; }
-        public string[] Reserved {  get; set; }
-        public bool BeingRepaired { get; set; }
+        public string GameName { get; set; } = "ukendt";
+        public string GameEdition { get; set; } = "ukendt";
+        public string Genre { get; set; } = "ukendt";
+        public int NumberOfPlayersMin { get; set; } = 0;
+        public int NumberOfPlayersMax { get; set; } = 0;
+        public int AgeMin { get; set; } = 0;
+        public double Price { get; set; } = 0;
+        public int Condition { get; set; } = 0;
+        public int QuantityOfGame { get; set; } = 0;
+        public string[] Waitlist { get; set; } = new string[0];
+        public string[] Reserved { get; set; } = new string[0];
+        public bool BeingRepaired { get; set; } = false;
 
 
-        public Game(string gameName, string gameEdition, string genre, int numberOfPlayersMin, int numberOfPlayersMax, double price, int condition, int quantityOfGame)
+        public Game(string gameName, string gameEdition, string genre, int numberOfPlayersMin, int numberOfPlayersMax, int ageMin, double price, int condition, int quantityOfGame, bool beingRepaired)
         {
             GameName = gameName;
             GameEdition = gameEdition;
             Genre = genre;
             NumberOfPlayersMin = numberOfPlayersMin;
             NumberOfPlayersMax = numberOfPlayersMax;
+            AgeMin = ageMin;
             Price = price;
             Condition = condition;
             QuantityOfGame = quantityOfGame;
+            BeingRepaired = beingRepaired;
         }
 
         public string MakeTitle()
         {
-            return GameName + ", " + GameEdition + ", " + Genre + ", " + NumberOfPlayersMin + ", " + NumberOfPlayersMax + ", " + Price + ", " + Condition + ", " + QuantityOfGame;
+            return GameName + ", " + GameEdition + ", " + Genre + ", " + NumberOfPlayersMin + ", " + NumberOfPlayersMax + ", " + AgeMin + ", " + Price + ", " + Condition + ", " + QuantityOfGame;
         }
     }
 }
