2e815175b9b19fd1596a59a043d3e0cff20afe87
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-04-09 21:58:18 +0200

    Udkommenteret waitlist

--- Genspil15/Waitlist.cs
+++ Genspil15/Waitlist.cs
@@ -11,23 +11,23 @@ namespace Genspil15
     {
         public Queue 
         
-        // Creating a Queue 
-        Queue myQueue = new Queue();
+        //// Creating a Queue 
+        //Queue myQueue = new Queue();
 
-        // Inserting the elements into the Queue 
-        myQueue.Enqueue("C#"); 
-        myQueue.Enqueue("PHP"); 
-        myQueue.Enqueue("Perl"); 
-        myQueue.Enqueue("Java"); 
-        myQueue.Enqueue("C"); 
+        //// Inserting the elements into the Queue 
+        //myQueue.Enqueue("C#"); 
+        //myQueue.Enqueue("PHP"); 
+        //myQueue.Enqueue("Perl"); 
+        //myQueue.Enqueue("Java"); 
+        //myQueue.Enqueue("C"); 
   
-        // Displaying the count of elements 
-        // contained in the Queue 
-        Console.Write("Total number of elements present in the Queue are: "); 
+        //// Displaying the count of elements 
+        //// contained in the Queue 
+        //Console.Write("Total number of elements present in the Queue are: "); 
   
-        Console.WriteLine(myQueue.Count); 
+        //Console.WriteLine(myQueue.Count); 
   
-        // Displaying the beginning element of Queue 
-        Console.WriteLine("Beginning Item is: " + myQueue.Peek()); 
+        //// Displaying the beginning element of Queue 
+        //Console.WriteLine("Beginning Item is: " + myQueue.Peek()); 
     }
 }
