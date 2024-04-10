d6f1a8b9dd6697071dbf69f048f4ae233d1f6d81
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-04-10 10:21:24 +0200

    Program læser nu SavedGamesList.txt og adder til den

--- Genspil15/Program.cs
+++ Genspil15/Program.cs
@@ -13,8 +13,9 @@ namespace Genspil15
 
 
 
-            string fileName = "sequence.txt"; //Navn på test fil til at indlæse fra listen
-            List<Game> games = new List<Game>(); //Laver en liste "games" af Game objecter
+            string fileName = "SavedGamesList.txt"; //Navn på test fil til at indlæse fra listen
+            List<Game> games = Filehandler.ReadGamesFromFile(fileName); //Læser testfilen og laver et liste object
+            
 
            
 
