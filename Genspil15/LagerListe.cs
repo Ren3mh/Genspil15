5d5b36a1bc4d227e93c46f4df11fa21ce1e38296
Gabriela Kucharska <gabi.kucharska250798@gmail.com>
2024-04-14 14:31:08 +0200

    Har rettet "bog" om til "spil", da jeg skrev forkert

--- Genspil15/LagerListe.cs
+++ Genspil15/LagerListe.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*class Bog
+/*class Spil
 {
     public string Navn { get; set; }
     public int Antal { get; set; }
     public string Stand { get; set; }
 
-    public Bog(string navn, int antal, string stand)
+    public Spil(string navn, int antal, string stand)
     {
         Navn = navn;
         Antal = antal;
@@ -22,53 +22,53 @@ using System.Threading.Tasks;
 class Program
 {
 
-    static void TilføjBog(Dictionary<string, List<Bog>> lagerliste, string genre, string navn, int antal, string stand)
+    static void TilføjSpil(Dictionary<string, List<Spil>> lagerliste, string genre, string navn, int antal, string stand)
     {
         if (!lagerliste.ContainsKey(genre))
         {
-            lagerliste.Add(genre, new List<Bog>());
+            lagerliste.Add(genre, new List<Spil>());
         }
-        lagerliste[genre].Add(new Bog(navn, antal, stand));
+        lagerliste[genre].Add(new Spil(navn, antal, stand));
     }
 
     static void Main()
     {
-        // En organiseret lagerliste af bøger fordelt på genre
-        Dictionary<string, List<Bog>> lagerliste = new Dictionary<string, List<Bog>>();
+        // En organiseret lagerliste af spil fordelt på genre
+        Dictionary<string, List<Spil>> lagerliste = new Dictionary<string, List<Spil>>();
 
-        // Her bliver bøgerne tilføjet til lagerlisten. Inklusiv gennre, stand og titler. 
+        // Her bliver spillene tilføjet til lagerlisten. Inklusiv genre, stand og titler. 
         // Man kan altid tilføje flere. De er tilføjet manuelt, da jeg ikke kunne få den til at læse fra en fil. 
-        TilføjBog(lagerliste, "Kids", "Original", 2, "God");
-        TilføjBog(lagerliste, "Kids", "Junior", 1, "Lidt skrammet");
-        TilføjBog(lagerliste, "Kids", "Giant", 2, "God");
+        TilføjSpil(lagerliste, "Kids", "Original", 2, "God");
+        TilføjSpil(lagerliste, "Kids", "Junior", 1, "Lidt skrammet");
+        TilføjSpil(lagerliste, "Kids", "Giant", 2, "God");
 
-        TilføjBog(lagerliste, "Rejsespil", "Berlin, Ticket To Ride", 3, "2 God, 1 til reparation");
-        TilføjBog(lagerliste, "Rejsespil", "Europe - engelsk", 0, "Ukendt");
-        TilføjBog(lagerliste, "Rejsespil", "Europe - dansk", 2, "God");
-        TilføjBog(lagerliste, "Rejsespil", "Europa - 1912", 0, "Ukendt");
-        TilføjBog(lagerliste, "Rejsespil", "Europe - engelsk", 0, "Ukendt");
-        TilføjBog(lagerliste, "Rejsespil", "First journey", 1, "God");
-        TilføjBog(lagerliste, "Rejsespil", "Ghost train", 0, "God");
-        TilføjBog(lagerliste, "Rejsespil", "Northern lights", 0, "Ukendt");
-        TilføjBog(lagerliste, "Rejsespil", "2nd edition cities", 1, "Dårlig");
-
-
-        TilføjBog(lagerliste, "Strategi", "Dansk 2nd edition", 1, "God");
-        TilføjBog(lagerliste, "Strategi", "Architects - dansk", 3, "2 God, 1 Uåbnet");
-        TilføjBog(lagerliste, "Strategi", "Architects - medals", 1, "Dårlig");
-        TilføjBog(lagerliste, "Strategi", "Dansk duel - Agora", 3, "1 God, 2 Uåbnet");
-        TilføjBog(lagerliste, "Strategi", "Duel - Agora - Engelsk", 1, "Dårlig");
+        TilføjSpil(lagerliste, "Rejsespil", "Berlin, Ticket To Ride", 3, "2 God, 1 til reparation");
+        TilføjSpil(lagerliste, "Rejsespil", "Europe - engelsk", 0, "Ukendt");
+        TilføjSpil(lagerliste, "Rejsespil", "Europe - dansk", 2, "God");
+        TilføjSpil(lagerliste, "Rejsespil", "Europa - 1912", 0, "Ukendt");
+        TilføjSpil(lagerliste, "Rejsespil", "Europe - engelsk", 0, "Ukendt");
+        TilføjSpil(lagerliste, "Rejsespil", "First journey", 1, "God");
+        TilføjSpil(lagerliste, "Rejsespil", "Ghost train", 0, "God");
+        TilføjSpil(lagerliste, "Rejsespil", "Northern lights", 0, "Ukendt");
+        TilføjSpil(lagerliste, "Rejsespil", "2nd edition cities", 1, "Dårlig");
 
 
-        // Her printes lagerlisten af bøger fordelt på genre
-        // Genrene udskrives med dets tilhørende bøger 
+        TilføjSpil(lagerliste, "Strategi", "Dansk 2nd edition", 1, "God");
+        TilføjSpil(lagerliste, "Strategi", "Architects - dansk", 3, "2 God, 1 Uåbnet");
+        TilføjSpil(lagerliste, "Strategi", "Architects - medals", 1, "Dårlig");
+        TilføjSpil(lagerliste, "Strategi", "Dansk duel - Agora", 3, "1 God, 2 Uåbnet");
+        TilføjSpil(lagerliste, "Strategi", "Duel - Agora - Engelsk", 1, "Dårlig");
+
+
+        // Her printes lagerlisten af spil fordelt på genre
+        // Genrene udskrives med deres tilhørende spil 
         Console.WriteLine("Lagerliste:");
         foreach (var genre in lagerliste)
         {
             Console.WriteLine($"Genre: {genre.Key}");
-            foreach (var bog in genre.Value)
+            foreach (var spil in genre.Value)
             {
-                Console.WriteLine($"- Bog: {bog.Navn}, Antal: {bog.Antal}, Stand: {bog.Stand}");
+                Console.WriteLine($"- Spil: {spil.Navn}, Antal: {spil.Antal}, Stand: {spil.Stand}");
             }
             Console.WriteLine();
         }
@@ -76,6 +76,4 @@ class Program
         Console.ReadKey();
     }
 
-
-}
 */
